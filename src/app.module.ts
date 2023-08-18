432cf71d73c5a807e0b59c34e5f435b323e166ec
Eric Sison <ericsison.dev@gmail.com>
2023-08-18 17:23:34 +0800

    added DrizzleModule and UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DrizzleModule } from './database/connection/drizzle.module';
+import { UserModule } from './api/users/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DrizzleModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          host: configService.getOrThrow('DB_HOST'),
+          port: parseInt(configService.getOrThrow('DB_PORT')),
+          user: configService.getOrThrow('DB_USER'),
+          password: configService.getOrThrow('DB_PASS'),
+          database: configService.getOrThrow('DB_NAME'),
+          synchronize: true,
+          drizzleConfigOptions: {
+            logger: true,
+          },
+        };
+      },
+    }),
+
+    UserModule,
+  ],
 })
 export class AppModule {}
