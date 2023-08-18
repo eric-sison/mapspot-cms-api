1dfd72f01180f413c3b53123086227429c3f8875
Eric Sison <ericsison.dev@gmail.com>
2023-08-18 17:29:13 +0800

    added script to execute drizzle migration

---
+++ src/database/scripts/migration.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Client } from 'pg';
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
+import { Logger } from '@nestjs/common';
+
+async function runMigration() {
+  // Initialize client object
+  const client = new Client({
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
+
+  // Establish connection
+  await client.connect();
+
+  // Initialize database object
+  const db = drizzle(client);
+
+  // Execute drizzle's migration command
+  await migrate(db, { migrationsFolder: './drizzle' });
+
+  // close the connection
+  client.end();
+
+  // Log the success message
+  Logger.log('Migration successful', 'DrizzleMigration');
+}
+
+runMigration();
